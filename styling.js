1b8b6cefee95f7e4cb07e2b8ee25d8d5bba94494
Raymond Tang <raymondtang1996@gmail.com>
2020-01-25 16:23:07 -0800

    fixed modal finally

--- styling.js
+++ styling.js
@@ -2,92 +2,94 @@
 var arrHead = new Array();
     arrHead = ['', 'Brand','Model', 'Color', 'Price','Purchase Date','Expiration Date','Quantity'];      // SIMPLY ADD OR REMOVE VALUES IN THE ARRAY FOR TABLE HEADERS.
 
-    // FIRST CREATE A TABLE STRUCTURE BY ADDING A FEW HEADERS AND
-    // ADD THE TABLE TO YOUR WEB PAGE.
-    function createTable() {
-        var empTable = document.createElement('table');
-        empTable.setAttribute('id', 'empTable');            // SET THE TABLE ID.
+// FIRST CREATE A TABLE STRUCTURE BY ADDING A FEW HEADERS AND
+// ADD THE TABLE TO YOUR WEB PAGE.
+function createTable() {
+    var empTable = document.createElement('table');
+    empTable.setAttribute('id', 'empTable');            // SET THE TABLE ID.
 
-        var tr = empTable.insertRow(-1);
+    var tr = empTable.insertRow(-1);
 
-        for (var h = 0; h < arrHead.length; h++) {
-            var th = document.createElement('th');          // TABLE HEADER.
-            th.innerHTML = arrHead[h];
-            tr.appendChild(th);
+    for (var h = 0; h < arrHead.length; h++) {
+        var th = document.createElement('th');          // TABLE HEADER.
+        th.innerHTML = arrHead[h];
+        tr.appendChild(th);
+    }
+
+    var div = document.getElementById('cont');
+    div.appendChild(empTable);    // ADD THE TABLE TO YOUR WEB PAGE.
+    addRow()
+}
+
+// ADD A NEW ROW TO THE TABLE.s
+function addRow() {
+    var empTab = document.getElementById('empTable');
+
+    var rowCnt = empTab.rows.length;        // GET TABLE ROW COUNT.
+    var tr = empTab.insertRow(rowCnt);      // TABLE ROW.
+    tr = empTab.insertRow(rowCnt);
+
+    for (var c = 0; c < arrHead.length; c++) {
+        var td = document.createElement('td');          // TABLE DEFINITION.
+        td = tr.insertCell(c);
+
+        if (c == 0) {           // FIRST COLUMN.
+            // ADD A BUTTON.
+            var button = document.createElement('input');
+
+            // SET INPUT ATTRIBUTE.
+            button.setAttribute('type', 'button');
+            button.setAttribute('value', 'Remove');
+
+            // ADD THE BUTTON's 'onclick' EVENT.
+            button.setAttribute('onclick', 'removeRow(this)');
+
+            td.appendChild(button);
         }
+        else {
+            // CREATE AND ADD TEXTBOX IN EACH CELL.
+            var ele = document.createElement('input');
+            ele.setAttribute('type', 'text');
+            ele.setAttribute('value', '');
 
-        var div = document.getElementById('cont');
-        div.appendChild(empTable);    // ADD THE TABLE TO YOUR WEB PAGE.
-        addRow()
+            td.appendChild(ele);
+        }
     }
+}
 
-    // ADD A NEW ROW TO THE TABLE.s
-    function addRow() {
-        var empTab = document.getElementById('empTable');
+// DELETE TABLE ROW.
+function removeRow(oButton) {
+    var empTab = document.getElementById('empTable');
+    empTab.deleteRow(oButton.parentNode.parentNode.rowIndex);       // BUTTON -> TD -> TR.
+}
 
-        var rowCnt = empTab.rows.length;        // GET TABLE ROW COUNT.
-        var tr = empTab.insertRow(rowCnt);      // TABLE ROW.
-        tr = empTab.insertRow(rowCnt);
+// EXTRACT AND SUBMIT TABLE DATA.
+function submit() {
+    var myTab = document.getElementById('empTable');
+    var values = new Array();
 
-        for (var c = 0; c < arrHead.length; c++) {
-            var td = document.createElement('td');          // TABLE DEFINITION.
-            td = tr.insertCell(c);
+    // LOOP THROUGH EACH ROW OF THE TABLE.
+    for (row = 1; row < myTab.rows.length - 1; row++) {
+        for (c = 0; c < myTab.rows[row].cells.length; c++) {   // EACH CELL IN A ROW.
 
-            if (c == 0) {           // FIRST COLUMN.
-                // ADD A BUTTON.
-                var button = document.createElement('input');
-
-                // SET INPUT ATTRIBUTE.
-                button.setAttribute('type', 'button');
-                button.setAttribute('value', 'Remove');
-
-                // ADD THE BUTTON's 'onclick' EVENT.
-                button.setAttribute('onclick', 'removeRow(this)');
-
-                td.appendChild(button);
-            }
-            else {
-                // CREATE AND ADD TEXTBOX IN EACH CELL.
-                var ele = document.createElement('input');
-                ele.setAttribute('type', 'text');
-                ele.setAttribute('value', '');
-
-                td.appendChild(ele);
+            var element = myTab.rows.item(row).cells[c];
+            if (element.childNodes[0].getAttribute('type') == 'text') {
+                values.push("'" + element.childNodes[0].value + "'");
             }
         }
     }
-
-    // DELETE TABLE ROW.
-    function removeRow(oButton) {
-        var empTab = document.getElementById('empTable');
-        empTab.deleteRow(oButton.parentNode.parentNode.rowIndex);       // BUTTON -> TD -> TR.
-    }
-
-    // EXTRACT AND SUBMIT TABLE DATA.
-    function submit() {
-        var myTab = document.getElementById('empTable');
-        var values = new Array();
-
-        // LOOP THROUGH EACH ROW OF THE TABLE.
-        for (row = 1; row < myTab.rows.length - 1; row++) {
-            for (c = 0; c < myTab.rows[row].cells.length; c++) {   // EACH CELL IN A ROW.
-
-                var element = myTab.rows.item(row).cells[c];
-                if (element.childNodes[0].getAttribute('type') == 'text') {
-                    values.push("'" + element.childNodes[0].value + "'");
-                }
-            }
-        }
-        
-        // SHOW THE RESULT IN THE CONSOLE WINDOW.
-        console.log(values);
-    }
+    
+    // SHOW THE RESULT IN THE CONSOLE WINDOW.
+    console.log(values);
+}
 
 // Get the modal
 var modal = document.getElementById("myModal");
+console.log(modal);
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
+console.log(btn);
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
